c572474c2e1e0ba9bf6445c0dd52d11d885352ba
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-02 18:12:32 -0300

    feat: diretor entity

--- src/main/java/org/swxtz/entities/Diretor.java
+++ src/main/java/org/swxtz/entities/Diretor.java
@@ -3,4 +3,16 @@ package org.swxtz.entities;
 public class Diretor {
     private String nome;
 
+    public Diretor(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }

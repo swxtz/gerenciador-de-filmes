ce1f3ecc82e01e3c34093f824dc90faf4a55ff6f
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-02 18:15:30 -0300

    feat: actor entity

---
+++ src/main/java/org/swxtz/entities/Ator.java
@@ -0,0 +1,18 @@
+package org.swxtz.entities;
+
+public class Ator {
+    private String nome;
+
+    public Ator(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
